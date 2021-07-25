a1c3be835901739e2bce7ff9510b8d5c57962d44
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-25 18:10:19 +0900

    user.rbのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tweets
+  # ユーザーを消したらユーザーの持つlikesも消える
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet
 end
