8d86fe76a2654ab4ca54fe817985c282905a0841
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-25 23:31:55 +0900

    tweetsコントローラー/new,createアクション追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,24 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.valid?
+      @tweet.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:tweet).merge(user_id: current_user.id)
   end
 end
