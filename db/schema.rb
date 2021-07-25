ab5cc9a9426bc4a9460f6cefb8017d16abdda197
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-25 19:14:08 +0900

    Tweetsテーブルにuser_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_084529) do
+ActiveRecord::Schema.define(version: 2021_07_25_101100) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_084529) do
     t.text "tweet", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
