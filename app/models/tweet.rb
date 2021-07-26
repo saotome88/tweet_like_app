64db5c328a077352320299a40eb54687c8a2b104
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-26 10:32:56 +0900

    tweet.rb/liked_usersを取得

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  # 親のtweetを消したら子のlikesも消える
-#  has_many :likes, dependent: :destroy
-#  has_many :liking_users, through: :likes, source: :user
+  has_many :likes
+  #「誰が」いいねをしたのかを取得する
+  has_many :liked_users, through: :likes, source: :user
 end
