87b556dc581e9e8f67302b2d66db12e334d42846
saotome88 <ryoji.saotome1988@gmail.com>
2021-07-26 10:30:54 +0900

    user.rbの修正/liked_tweetsを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets, dependent: :destroy
-  # ユーザーを消したらユーザーの持つlikesも消える
-#  has_many :likes, dependent: :destroy
-#  has_many :like_tweets, through: :likes, source: :tweet
+  has_many :likes, dependent: :destroy
+  # ユーザーがどの投稿に「いいねをした」かを取得する
+  has_many :liked_tweets, through: :likes, source: :tweet
 end
